1957be460275df096e3f3c3c420d428f2bf4e3af
root <root@desarrollo.novalocal>
2020-05-17 19:24:09 +0000

    añadido

--- wordpress/wp-content/themes/estore/js/metabox-toggle.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready( function() {
-	jQuery('#page_template').change(function() {
-		jQuery('#woocommerce-category').toggle(jQuery(this).val() == 'page-templates/template-wc-collection.php');
-	}).change();
-});
