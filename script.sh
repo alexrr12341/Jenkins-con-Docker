d347d9db89160f6b93755240f6bd337121458958
root <root@desarrollo.novalocal>
2020-05-18 17:11:20 +0000

    script modificado

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 5
 docker exec mariadb mysql -h mariadb -u wordpress -pwordpress wordpress -e "UPDATE wp_options SET option_value='http://jenkins' Where option_name='siteurl'"
 docker exec mariadb mysql -h mariadb -u wordpress -pwordpress wordpress -e "UPDATE wp_options SET option_value='http://jenkins' Where option_name='home'"
 
