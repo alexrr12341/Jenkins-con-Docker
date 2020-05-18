9cc1838fcdedacd22a6ea411845fadcf2cf071f9
root <root@desarrollo.novalocal>
2020-05-18 16:52:08 +0000

    prueb4

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker exec mariadb mysql -u wordpress -pwordpress wordpress -e "UPDATE wp_options SET option_value='http://jenkins' Where option_name='siteurl'"
-docker exec mariadb mysql -u wordpress -pwordpress wordpress -e "UPDATE wp_options SET option_value='http://jenkins' Where option_name='home'"
+docker exec mariadb mysql -h mariadb -u wordpress -pwordpress wordpress -e "UPDATE wp_options SET option_value='http://jenkins' Where option_name='siteurl'"
+docker exec mariadb mysql -h mariadb -u wordpress -pwordpress wordpress -e "UPDATE wp_options SET option_value='http://jenkins' Where option_name='home'"
 
