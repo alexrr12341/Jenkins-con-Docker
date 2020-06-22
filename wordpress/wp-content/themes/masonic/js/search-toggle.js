893cbbe4971a3b0b498020725092dc8ff4f976da
Jenkins <jenkins@jenkins.novalocal>
2020-06-22 07:58:57 +0000

    Jenkins Automatico

---
+++ wordpress/wp-content/themes/masonic/js/search-toggle.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () {
+    jQuery('.sb-search').click(function () {
+        jQuery('.masonic-search-toggle').slideToggle('slow');
+    });
+});
+jQuery(document).ready(function () {
+    jQuery('.sb-search-res').click(function () {
+        jQuery('.masonic-search-toggle').slideToggle('slow');
+    });
+});
