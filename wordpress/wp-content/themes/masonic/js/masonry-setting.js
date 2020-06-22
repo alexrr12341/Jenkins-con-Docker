5cea323199a74c040606695edcda810216e533a8
root <root@jenkins.novalocal>
2020-06-22 07:37:59 +0000

    wp update

--- wordpress/wp-content/themes/masonic/js/masonry-setting.js
+++
@@ -1,6 +0,0 @@
-// the main masonry jquery setting to call the masonry script
-jQuery(window).load(function () {
-   jQuery('#container').masonry({
-      itemSelector: '.post-container'
-   });
-});
